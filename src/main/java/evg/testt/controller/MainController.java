d45fe11ecec0e92ebb3928b00732ffef8e1c1ac3
Ievgen Grabets <i.grabets@aimprosoft.com>
2015-11-26 19:05:36 +0200

    Initial commit

---
+++ src/main/java/evg/testt/controller/MainController.java
@@ -0,0 +1,21 @@
+package evg.testt.controller;
+
+import evg.testt.util.JspPath;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created on 9/10/15.
+ */
+@Controller
+public class MainController {
+
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView showAll() {
+        return new ModelAndView(JspPath.HOME);
+    }
+
+}
